7d6bee260969c3865b9d57abbba092d4de30390c
amm926616 <amm926616@gmail.com>
2023-09-10 15:07:41 +0630

    updated

--- script.js
+++ script.js
@@ -26,8 +26,81 @@ const initialBoard = [
 ];
 
 
+// Define an array to store coordinates for each square
+const coordinates = [
+    ['a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8', 'h8'],
+    ['a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7'],
+    ['a6', 'b6', 'c6', 'd6', 'e6', 'f6', 'g6', 'h6'],
+    ['a5', 'b5', 'c5', 'd5', 'e5', 'f5', 'g5', 'h5'],
+    ['a4', 'b4', 'c4', 'd4', 'e4', 'f4', 'g4', 'h4'],
+    ['a3', 'b3', 'c3', 'd3', 'e3', 'f3', 'g3', 'h3'],
+    ['a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2'],
+    ['a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1'],
+];
+
+
+let draggedImage;
+
+function dragStart(e) {
+    draggedImage = e.target;
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragDrop(e) {
+    e.preventDefault();
+    const targetSquare = e.target;
+    console.log("target square", targetSquare);
+
+    if (targetSquare.tagName === "DIV") {
+        targetSquare.appendChild(draggedImage);
+        console.log("****", draggedImage, "on", targetSquare, "****");
+    } else if (targetSquare.tagName === "IMG") {
+        targetSquare.replaceWith(draggedImage);
+        console.log("****", targetSquare, "is replaced with", draggedImage, "****")
+    }
+}
+
 function createChessBoard() {
     const chessboard = document.getElementById("chessboard");
+
+    // Define an array to store coordinates for each square
+    const coordinates = [
+        ['a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8', 'h8'],
+        ['a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7'],
+        ['a6', 'b6', 'c6', 'd6', 'e6', 'f6', 'g6', 'h6'],
+        ['a5', 'b5', 'c5', 'd5', 'e5', 'f5', 'g5', 'h5'],
+        ['a4', 'b4', 'c4', 'd4', 'e4', 'f4', 'g4', 'h4'],
+        ['a3', 'b3', 'c3', 'd3', 'e3', 'f3', 'g3', 'h3'],
+        ['a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2'],
+        ['a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1'],
+    ];
+
+    // to define the type of pieces
+    const pieceTypes = [
+        ["rook", "knight", "bishop", "queen", "king", "bishop", "knight", "rook"],
+        ["p", "p", "p", "p", "p", "p", "p", "p"],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["p", "p", "p", "p", "p", "p", "p", "p"],
+        ["rook", "knight", "bishop", "queen", "king", "bishop", "knight", "rook"]
+    ]
+
+    // to define team
+    const teamColors = [
+        ["w", "w", "w", "w", "w", "w", "w", "w"],
+        ["w", "w", "w", "w", "w", "w", "w", "w"],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["b", "b", "b", "b", "b", "b", "b", "b"],
+        ["b", "b", "b", "b", "b", "b", "b", "b"]
+    ]
 
     for (let row = 0; row < 8; row++) {
         for (let column = 0; column < 8; column++) {
@@ -39,18 +112,38 @@ function createChessBoard() {
                 square.classList.add("black");
             }
 
-            pieceCode = initialBoard[row][column];
+            // Adding event listener to each square
+            square.addEventListener('dragover', dragOver);
+            square.addEventListener('drop', dragDrop);
+
+            // Create an inner container div for the image
+            const imageContainer = document.createElement('div');
+            imageContainer.classList.add('image-container');
+
+            const pieceCode = initialBoard[row][column];
             if (pieceCode) {
                 const pieceImage = document.createElement('img');
-                pieceImage.src = pieceImages[pieceCode];                
-                square.appendChild(pieceImage);
+                pieceImage.src = pieceImages[pieceCode];
+                pieceImage.draggable = true; // Make the image draggable
+                pieceImage.setAttribute("type", pieceTypes[row][column]);
+                pieceImage.setAttribute("team-color", teamColors[row][column]);
+                imageContainer.appendChild(pieceImage);
+
+                // Adding event listener to image
+                pieceImage.addEventListener('dragstart', dragStart);
             }
 
-            chessboard.appendChild(square);
+            square.appendChild(imageContainer);
 
+            // Set the data-coordinate attribute to store the coordinate
+            square.setAttribute('coordinate', coordinates[row][column]);
+
+            chessboard.appendChild(square);
         }
     }
 }
+
+
 
 createChessBoard();
 
