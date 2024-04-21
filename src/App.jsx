34e552003dbe07b30476f00300d672761d62e921
amm926616 <amm926616@gmail.com>
2024-04-21 16:54:05 +0630

    updated website

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import image1 from './assets/imgs/0017.jpg';
+import image2 from './assets/imgs/0018.jpg';
+import image3 from './assets/imgs/0013.jpg';
+
+function App() {
+  return (
+    <>
+      <h3>You are a worthless piece of shit. Your life is worthless. You better die off.</h3>
+      <ul>
+        <li><a href={image1} target="_blank" rel="noopener noreferrer">Feed me daddy</a></li>
+        <li><a href={image2} target="_blank" rel="noopener noreferrer">Taste so sweet</a></li>
+        <li><a href={image3} target="_blank" rel="noopener noreferrer">Ahh! It hurts</a></li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
